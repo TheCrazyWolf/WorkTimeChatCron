44d61f32d2bce9d6ffd815e31cc7128895097965
TheCrazyWolf <tcrazywolf63@gmail.com>
2025-07-07 17:49:43 +0400

    update model

--- WorkTimeChat.Models/WorkTimeConfig.cs
+++ WorkTimeChat.Models/WorkTimeConfig.cs
@@ -2,7 +2,7 @@
 
 public class WorkTimeConfig
 {
-    public long ChatId { get; set; }
+    public long[] ChatId { get; set; } = null!;
     public string AccessToken { get; set; } = null!;
     public string ChatTurnOnMessage { get; set; } = null!;
     public string[] JobTurnOnParams { get; set; } = null!;
