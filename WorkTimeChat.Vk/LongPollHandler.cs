7f0385273f0ed0705d18f157855d836249432870
TheCrazyWolf <tcrazywolf63@gmail.com>
2025-07-21 00:23:27 +0400

    обертка в try

--- WorkTimeChat.Vk/LongPollHandler.cs
+++ WorkTimeChat.Vk/LongPollHandler.cs
@@ -13,61 +13,68 @@ public class LongPollHandler(VkBotWorker vkBot, IConfiguration configuration, Ch
 
         while (true) // Бесконечный цикл, получение обновлений
         {
-            var config = configuration.Get<WorkTimeConfig>();
-            var s = bot.Groups.GetLongPollServer(config!.GroupId);
-            var poll = bot.Groups.GetBotsLongPollHistory(
-                new BotsLongPollHistoryParams()
-                {
-                    Server = s.Server, Ts = s.Ts, Key = s.Key, Wait = 25
-                });
-            if (poll?.Updates == null) continue;
-
-            foreach (var msg in from a in poll.Updates
-                     where a.Type.Value == GroupUpdateType.MessageNew
-                     select ((MessageNew)a.Instance).Message!)
+            try
             {
-                if (msg.Text.Contains("identity"))
-                {
-                    var parm = new MessagesSendParams()
+                var config = configuration.Get<WorkTimeConfig>();
+                var s = bot.Groups.GetLongPollServer(config!.GroupId);
+                var poll = bot.Groups.GetBotsLongPollHistory(
+                    new BotsLongPollHistoryParams()
                     {
-                        PeerId = msg.PeerId,
-                        Message = $"Индентификатор этого чата: {msg.PeerId}",
-                        RandomId = new Random().Next(),
-                    };
-                    await bot.Messages.SendAsync(parm);
-                    return;
-                }
+                        Server = s.Server, Ts = s.Ts, Key = s.Key, Wait = 25
+                    });
+                if (poll?.Updates == null) continue;
 
-                if (!config.ChatIds.Contains((long)msg.PeerId!))
+                foreach (var msg in from a in poll.Updates
+                         where a.Type.Value == GroupUpdateType.MessageNew
+                         select ((MessageNew)a.Instance).Message!)
                 {
-                    continue;
-                }
+                    if (msg.Text.Contains("identity"))
+                    {
+                        var parm = new MessagesSendParams()
+                        {
+                            PeerId = msg.PeerId,
+                            Message = $"Индентификатор этого чата: {msg.PeerId}",
+                            RandomId = new Random().Next(),
+                        };
+                        await bot.Messages.SendAsync(parm);
+                        return;
+                    }
 
-                var isWorkTime = chatWorkTimeService.IsWorkingTime(DateTime.Now);
-                var isAllowedUser = config.AllowedUsersIdAlways.Contains((long)msg.FromId!);
+                    if (!config.ChatIds.Contains((long)msg.PeerId!))
+                    {
+                        continue;
+                    }
 
-                if (isWorkTime) return;
-                if (isAllowedUser) return;
-                
-                var history = await bot.Messages.GetHistoryAsync(new MessagesGetHistoryParams()
-                    { PeerId = msg.PeerId });
+                    var isWorkTime = chatWorkTimeService.IsWorkingTime(DateTime.Now);
+                    var isAllowedUser = config.AllowedUsersIdAlways.Contains((long)msg.FromId!);
 
-                var msgIdsToBeDeleted = history.Messages
-                    .Where(x => !chatWorkTimeService.IsWorkingTime(x.Date!.Value.AddHours(4)))
-                    .Select(x=> Convert.ToUInt64(x.Id)).ToList();
-                
-                if (!msgIdsToBeDeleted.Any()) continue;
-                
-                try
-                {
-                    var result = await bot.Messages.DeleteAsync(
-                        messageIds: msgIdsToBeDeleted, null, (ulong)msg.PeerId, true
-                    );
+                    if (isWorkTime) return;
+                    if (isAllowedUser) return;
+
+                    var history = await bot.Messages.GetHistoryAsync(new MessagesGetHistoryParams()
+                        { PeerId = msg.PeerId });
+
+                    var msgIdsToBeDeleted = history.Messages
+                        .Where(x => !chatWorkTimeService.IsWorkingTime(x.Date!.Value.AddHours(4)))
+                        .Select(x => Convert.ToUInt64(x.Id)).ToList();
+
+                    if (!msgIdsToBeDeleted.Any()) continue;
+
+                    try
+                    {
+                        var result = await bot.Messages.DeleteAsync(
+                            messageIds: msgIdsToBeDeleted, null, (ulong)msg.PeerId, true
+                        );
+                    }
+                    catch (Exception e)
+                    {
+                        //
+                    }
                 }
-                catch (Exception e)
-                {
-                    //
-                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
             }
         }
     }
